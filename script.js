32d52708f35e31ed6d18e12d58f58e6206c4b265
Viktor Koleski <viktor.koleski@gmail.com>
2023-12-24 23:01:37 +0100

    Added Monthly/Yearly switch

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const overlay = document.getElementById("overlay");
-const basic = document.getElementById("basic");
-const premium = document.getElementById("premium");
-const enterprise = document.getElementById("enterprise");
-const btnBasic = document.getElementById("btnBasic");
-const btnPremium = document.getElementById("btnPremium");
-const btnEnterprise = document.getElementById("btnEnterprise");
+const basic = document.querySelectorAll(".basic");
+const premium = document.querySelectorAll(".premium");
+const enterprise = document.querySelectorAll(".enterprise");
+const btnBasic = document.querySelectorAll(".btnBasic");
+const btnPremium = document.querySelectorAll(".btnPremium");
+const btnEnterprise = document.querySelectorAll(".btnEnterprise");
 const planValue = document.getElementById("planValue");
+const monthly = document.getElementById("monthly");
+const yearly = document.getElementById("yearly");
+
 
 function showModal() {
     overlay.style.display = "flex";
@@ -16,32 +19,59 @@ function closeModal() {
 }
 
 function selectBasic() {
-    basic.style.scale = "1.2";
-    btnBasic.classList.add("btnSelected");
-    btnPremium.classList.remove("btnSelected");
-    btnEnterprise.classList.remove("btnSelected");
-    planValue.value = "basic";
-    premium.style.scale = "1";
-    enterprise.style.scale = "1";
+    for(let i = 0; i < basic.length; i++){
+        basic[i].style.scale = "1.2";
+        btnBasic[i].classList.add("btnSelected");
+        btnPremium[i].classList.remove("btnSelected");
+        btnEnterprise[i].classList.remove("btnSelected");
+        planValue.value = "basic";
+        premium[i].style.scale = "1";
+        enterprise[i].style.scale = "1";;
+    } 
 }
 
 function selectPremium() {
-    premium.style.scale = "1.2";
-    btnPremium.classList.add("btnSelected");
-    btnBasic.classList.remove("btnSelected");
-    btnEnterprise.classList.remove("btnSelected");
-    planValue.value = "premium";
-    basic.style.scale = "1";
-    enterprise.style.scale = "1";
+    for(let i = 0; i < basic.length; i++){
+        premium[i].style.scale = "1.2";
+        btnPremium[i].classList.add("btnSelected");
+        btnBasic[i].classList.remove("btnSelected");
+        btnEnterprise[i].classList.remove("btnSelected");
+        planValue.value = "premium";
+        basic[i].style.scale = "1";
+        enterprise[i].style.scale = "1";
+    }
 }
 
 function selectEnterprise() {
-    enterprise.style.scale = "1.2";
-    btnEnterprise.classList.add("btnSelected");
-    btnBasic.classList.remove("btnSelected");
-    btnPremium.classList.remove("btnSelected");
-    planValue.value = "enterprise";
-    basic.style.scale = "1";
-    premium.style.scale = "1";
+    for(let i = 0; i < basic.length; i++){
+        enterprise[i].style.scale = "1.2";
+        btnEnterprise[i].classList.add("btnSelected");
+        btnBasic[i].classList.remove("btnSelected");
+        btnPremium[i].classList.remove("btnSelected");
+        planValue.value = "enterprise";
+        basic[i].style.scale = "1";
+        premium[i].style.scale = "1";
+    }
 }
+
+
+function billingSwitch() {
+    const planMonthly = document.getElementById("plansMonthly");
+    const planYearly = document.getElementById("plansYearly");
+    if(monthly.checked === true && yearly.checked === false){
+        planMonthly.style.display = "flex";
+        planYearly.style.display = "none";
+    } else {
+        planMonthly.style.display = "none";
+        planYearly.style.display = "flex";
+    }
+}
+billingSwitch();
+monthly.addEventListener("change", billingSwitch);
+yearly.addEventListener("change", billingSwitch);
+
+
+
+
+
 
